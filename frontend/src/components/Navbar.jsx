052d06c2fcc0ca116388dd5e7bfcb4a984d8051c
Thanigaivel <119962410+thanigaivel14@users.noreply.github.com>
2025-05-31 22:09:31 +0530

    Update Navbar.jsx avatart changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
     localStorage.removeItem("user");
   };
 
-  const imguri = `http://localhost:8000/api/uploads/${user?.avatar}`;
+  const imguri = `https://studybuddy-idnp.onrender.com/api/uploads/${user?.avatar}`;
 
   return (
     <nav className="fixed top-0 left-0 w-full z-50 backdrop-blur-lg bg-white/20 border-b border-white/30 shadow-lg px-4 sm:px-8 py-3 sm:py-4 flex flex-col sm:flex-row items-center justify-between">
