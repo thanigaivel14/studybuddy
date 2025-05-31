455245c68133172c25d4489d70f355d23b7332bf
Thanigaivel <119962410+thanigaivel14@users.noreply.github.com>
2025-05-31 17:30:59 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API =axios.create({
-    baseURL:"http://localhost:8000/api",
+   baseURL: "https://studybuddy-idnp.onrender.com/api",
     withCredentials:true,
 })
 export default API;
